ce250c1c61d2c0f154524080ecd165469f505bf5
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-03-01 13:43:34 +0530

    Update TwoSum.py

--- Qsn1/TwoSum.py
+++ Qsn1/TwoSum.py
@@ -3,5 +3,5 @@ class Solution:
         for i in nums[0:-1]:
             if target - i in nums[nums.index(i)+1:]:
                 num = nums[nums.index(i)+1:]
-                return [nums.index(i),nums.index(i)+num.index(target - i) + 1]
+                return [nums.index(i) , nums.index(i)+num.index(target - i) + 1]
                 break
