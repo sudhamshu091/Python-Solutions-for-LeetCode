83c5b958f9df8592af1d29b355a6168884a9bb42
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-03-01 22:36:02 +0530

    Update lengthOfLongestSubstring.py

--- Qsn3/lengthOfLongestSubstring.py
+++ Qsn3/lengthOfLongestSubstring.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def lengthOfLongestSubstring(self, string: str) -> int:
+        if len(string) == 0:
+            return 0
+        longest_string = string[0]
+        len_longest_string = 1
+        longest = 1
+        for i in string[1:]:
+            if i not in longest_string:
+                longest_string += i
+                len_longest_string += 1
+                if len_longest_string > longest:
+                    longest = len_longest_string
+            else:
+                while i in longest_string:
+                    longest_string = longest_string[1:]
+                    len_longest_string -= 1
+                longest_string += i  
+                len_longest_string += 1
+        return longest
