b7676151a3c4a01c85aa893abca78b7ee477ef12
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-03-01 13:42:09 +0530

    Update TwoSum.py

--- Qsn1/TwoSum.py
+++ Qsn1/TwoSum.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in nums[0:-1]:
+            if target - i in nums[nums.index(i)+1:]:
+                num = nums[nums.index(i)+1:]
+                return [nums.index(i),nums.index(i)+num.index(target - i) + 1]
+                break
