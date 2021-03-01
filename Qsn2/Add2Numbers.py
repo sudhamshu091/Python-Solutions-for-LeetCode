5b082f03db6a1f3573217a0789e485d71b70e21a
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-03-01 11:51:14 +0530

    Update Add2Numbers.py

--- Qsn2/Add2Numbers.py
+++ Qsn2/Add2Numbers.py
@@ -1,1 +1,34 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        current = ListNode(0)
+        temp = current
+        carry = 0
+        
+        while l1 != None or l2 != None:
+            
+            x,y = 0,0
+            
+            if(l1):
+                x = l1.val
+                l1 = l1.next
+                
+            if(l2):
+                y = l2.val
+                l2 = l2.next
+                
+            sum = carry + x + y
+            carry = sum // 10
+            
+            current.next = ListNode(sum%10)
+            current = current.next
+    
+        if(carry):
+            current.next = ListNode(carry)      
+        
+        return temp.next
